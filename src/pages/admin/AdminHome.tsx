edc6741b64d9a305aa0005c5cb008fc481fda6c2
Oleg <scmr1@mail.ru>
2022-06-10 19:23:05 +0300

    day 2

---
+++ src/pages/admin/AdminHome.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+interface IAdminHomeProps {
+}
+export const AdminHome: React.FC<IAdminHomeProps> = () => {
+    return (
+        <h1>q234</h1>
+        
+    )
+}
---
+++ src/pages/admin/UsersList.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+interface IUsersListProps {
+}
+export const UsersList: React.FC<IUsersListProps> = () => {
+    return (
+        <h1>ququ</h1>
+        
+    )
+}
